57b331bc79ee6a7a435cd7f607d0a8379e85be9f
Sebastian OM <sebastianobandomorales@gmail.com>
2023-03-31 11:34:49 -0500

    starting

---
+++ src/components/columns.js
@@ -0,0 +1,26 @@
+export const COLUMNS = [
+    {
+        Header: 'Id',
+        accessor: 'id'
+    },
+    {
+        Header: 'First Name',
+        accesor: 'first_name'
+    },
+    {
+        Header: 'Last Name',
+        accesor: 'last_name'
+    },
+    {
+        Header: 'Date of Birth',
+        accessor: 'date_of_birth'
+    },
+    {
+        Header: 'Country',
+        accesor: 'country'
+    },     
+    {
+        Header: 'Phone',
+        accesor: 'phone'
+    },                     
+]
